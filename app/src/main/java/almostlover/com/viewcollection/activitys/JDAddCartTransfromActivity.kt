287bcc949eff76bc11067825cc775d7f25ef91f7
廖鹏辉 <467969@qq.com>
2019-04-09 15:52:17 +0800

    添加购物车动画  下一步完成greendao

--- app/src/main/java/almostlover/com/viewcollection/activitys/JDAddCartTransfromActivity.kt
+++ app/src/main/java/almostlover/com/viewcollection/activitys/JDAddCartTransfromActivity.kt
@@ -1,30 +1,57 @@
 package almostlover.com.viewcollection.activitys
 
-import almostlover.com.viewcollection.R
-import android.support.v7.app.AppCompatActivity
+import almostlover.com.viewcollection.views.addcartview.cart_type_evaluator.AddCartTypeEnvaluator
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
+import android.graphics.PointF
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import android.view.View
+import android.view.animation.AnticipateInterpolator
+import android.view.animation.AnticipateOvershootInterpolator
+import android.view.animation.BounceInterpolator
 import com.bumptech.glide.Glide
-import com.bumptech.glide.load.resource.bitmap.CircleCrop
 import com.bumptech.glide.request.RequestOptions
-import jp.wasabeef.glide.transformations.CropCircleTransformation
 import kotlinx.android.synthetic.main.activity_jdadd_cart_transfrom.*
+
 
 class JDAddCartTransfromActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_jdadd_cart_transfrom)
-        Glide.with(this).load("http://img14.360buyimg.com/n0/jfs/t1/32472/22/3371/192059/5c74b147Ee2eaefb4/8a9eaa0ea1365ed0.jpg").into(iv_bg)
+        setContentView(almostlover.com.viewcollection.R.layout.activity_jdadd_cart_transfrom)
+//        Glide.with(this).load("http://img14.360buyimg.com/n0/jfs/t1/32472/22/3371/192059/5c74b147Ee2eaefb4/8a9eaa0ea1365ed0.jpg").into(iv_bg)
         iv_cart.setOnClickListener {
 
             Glide.with(this).load("http://img14.360buyimg.com/n0/jfs/t1/32472/22/3371/192059/5c74b147Ee2eaefb4/8a9eaa0ea1365ed0.jpg").apply(
-                RequestOptions.circleCropTransform()).into(iv_move)
+                RequestOptions()
+                    .circleCrop()).into(iv_move)
             iv_move.visibility = View.VISIBLE
 
+//            val scaleAnimation = AnimationUtils.loadAnimation(this, R.anim.scale_hide)
+//            iv_move.animation = scaleAnimation
+//            iv_move.startAnimation(scaleAnimation)
 
+            val animatorSet = AnimatorSet()
 
+            val animatorX = ObjectAnimator.ofFloat(iv_move, "scaleX", 1f, 0.1f)
+            val animatorY = ObjectAnimator.ofFloat(iv_move, "scaleY", 1f, 0.1f)
+            animatorX.setInterpolator ( AnticipateInterpolator() )
+            animatorY.setInterpolator ( AnticipateInterpolator() )
+            val controlPonit = PointF(0f, 0F)
+            val ofObject = ObjectAnimator.ofObject(
+                iv_move,
+                "mPointF",
+                AddCartTypeEnvaluator(controlPonit),
+                PointF(iv_move.x, iv_move.y),
+                PointF(iv_cart.x, iv_cart.y)
+            )
+            animatorX.duration =10000
+            animatorY.duration =10000
+            ofObject.duration = 10000
 
+            animatorSet.play(animatorX).with(animatorY).before(ofObject)
+            animatorSet.start()
         }
     }
 }
