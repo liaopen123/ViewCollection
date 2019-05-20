141727ee254a676467e09f6777086c06862da426
liaopen123 <467969@qq.com>
2019-05-20 16:54:53 +0800

    添加了一种方法  动态addview的

--- app/src/main/java/almostlover/com/viewcollection/utils/lifecyclercallback/ImpLifeCyclerCallBack.kt
+++ app/src/main/java/almostlover/com/viewcollection/utils/lifecyclercallback/ImpLifeCyclerCallBack.kt
@@ -5,6 +5,7 @@ import android.app.Application
 import android.graphics.Color
 import android.os.Bundle
 import android.view.ViewGroup
+import android.view.ViewTreeObserver
 import android.widget.LinearLayout
 import android.widget.TextView
 
@@ -12,32 +13,11 @@ import android.widget.TextView
 class ImpLifeCyclerCallBack : Application.ActivityLifecycleCallbacks {
     override fun onActivityPaused(activity: Activity?) {
 
-        try {
-            val textView = TextView(activity)
-            //这里的Textview的父layout是ListView，所以要用ListView.LayoutParams
-
-
-            val layoutParams =
-                LinearLayout.LayoutParams(
-                    LinearLayout.LayoutParams.WRAP_CONTENT,
-                    LinearLayout.LayoutParams.WRAP_CONTENT
-                )
-            textView.layoutParams = layoutParams
-            textView.setPadding(20, 0, 20, 0)
-            textView.text = "我是后期添加的文字"
-            textView.setTextColor(Color.BLACK)
-            textView.textSize = 40f
-            val childAt = activity?.findViewById<ViewGroup>(android.R.id.content)
-            val childAt1 = childAt?.getChildAt(0)
-            val vg = childAt1 as ViewGroup
-            vg.addView(textView)
-        } catch (e: Exception) {
-            e.printStackTrace()
-        }
 
     }
 
     override fun onActivityResumed(activity: Activity?) {
+
     }
 
     override fun onActivityStarted(activity: Activity?) {
@@ -53,8 +33,35 @@ class ImpLifeCyclerCallBack : Application.ActivityLifecycleCallbacks {
     }
 
     override fun onActivityCreated(activity: Activity?, savedInstanceState: Bundle?) {
+        if (activity != null) {
+            initOnLayoutListener(activity)
+        }
 
+    }
 
+    private fun initOnLayoutListener(activity: Activity) {
+        val viewTreeObserver = activity.window.decorView.viewTreeObserver
+        viewTreeObserver.addOnGlobalLayoutListener(object : ViewTreeObserver.OnGlobalLayoutListener {
+            override fun onGlobalLayout() {
+                val textView = TextView(activity)
+                //这里的Textview的父layout是ListView，所以要用ListView.LayoutParams
+                val layoutParams =
+                    LinearLayout.LayoutParams(
+                        LinearLayout.LayoutParams.WRAP_CONTENT,
+                        LinearLayout.LayoutParams.WRAP_CONTENT
+                    )
+                textView.layoutParams = layoutParams
+                textView.setPadding(20, 0, 20, 0)
+                textView.text = "我是后期添加的文字"
+                textView.setTextColor(Color.BLACK)
+                textView.textSize = 40f
+                val childAt = activity?.findViewById<ViewGroup>(android.R.id.content)
+                val childAt1 = childAt?.getChildAt(0)
+                val vg = childAt1 as ViewGroup
+                vg.addView(textView)
+                activity.window.decorView.viewTreeObserver.removeOnGlobalLayoutListener(this)
+            }
+        })
     }
 
 }
